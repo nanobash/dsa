31bb13c5417d33bdf64d1c53ec680fb6e3d98f63
Tornike Nanobashvili <tornike.nanobashvili@paxful.com>
2021-01-04 23:53:45 +0200

    Implements selection sort algorithm
    	Selection sort is considered to be generally more effective than bubble sort,
    	as it does swapping only once per sub iteration, compare to bubble sort.
    	However selection sort does not guarantees stable sorting, hence
    	selection sort is an unstable sort algorithm.
    
    	Time complexity: O(n ^ 2), O of n ^ 2
    
    	Space complexity: This is an in-place algorithm, as extra
    	memory that is needed doesn't depend on the number of items
    	that are going to be sorted.

--- src/acme/algorithms/Sort.java
+++ src/acme/algorithms/Sort.java
@@ -2,6 +2,88 @@ package acme.algorithms;
 
 public class Sort {
     /**
+     * Selection sort is considered to be generally more effective than bubble sort, as it does swapping only once
+     *      per sub iteration, compare to bubble sort. However selection sort does not guarantees stable sorting, hence
+     *      selection sort is an unstable sort algorithm.
+     *
+     * Selection sort sorts on array in asc order,
+     *      by memorizing larger value of the array and swaps with the last unsorted index.
+     *
+     * Selection sort divides an array into sorted and unsorted partitions.
+     *
+     * Selection sort is an unstable algorithm, meaning that it does not preserve relative ordering of duplicate items.
+     *
+     * Time complexity: O(n ^ 2), O of n ^ 2
+     *
+     * Space complexity: This is an in-place algorithm, as extra memory that is needed doesn't depend
+     *      on the number of items that are going to be sorted.
+     *
+     * @param array unsorted array
+     *
+     * @return sorted array
+     */
+    public static int[] selectionSortAsc(int[] array) {
+        int lastUnsortedIdx = array.length - 1;
+
+        while (lastUnsortedIdx != 0) {
+            int largestItemIdx = 0;
+
+            for (int i = 0; i <= lastUnsortedIdx; ++i) {
+                if (array[i] > array[largestItemIdx]) {
+                    largestItemIdx = i;
+                }
+            }
+
+            swap(array, largestItemIdx, lastUnsortedIdx);
+
+            --lastUnsortedIdx;
+        }
+
+        return array;
+    }
+
+    /**
+     * Selection sort is considered to be generally more effective than bubble sort, as it does swapping only once
+     *      per sub iteration, compare to bubble sort. However selection sort does not guarantees stable sorting, hence
+     *      selection sort is an unstable sort algorithm.
+     *
+     * Selection sort sorts on array in desc order,
+     *      by memorizing larger value of the array and swaps with the last unsorted index.
+     *
+     * Selection sort divides an array into sorted and unsorted partitions.
+     *
+     * Selection sort is an unstable algorithm, meaning that it does not preserve relative ordering of duplicate items.
+     *
+     * Time complexity: O(n ^ 2), O of n ^ 2
+     *
+     * Space complexity: This is an in-place algorithm, as extra memory that is needed doesn't depend
+     *      on the number of items that are going to be sorted.
+     *
+     * @param array unsorted array
+     *
+     * @return sorted array
+     */
+    public static int[] selectionSortDesc(int[] array) {
+        int lastUnsortedIdx = array.length - 1;
+
+        while (lastUnsortedIdx != 0) {
+            int largestItemIdx = 0;
+
+            for (int i = 0; i <= lastUnsortedIdx; ++i) {
+                if (array[i] < array[largestItemIdx]) {
+                    largestItemIdx = i;
+                }
+            }
+
+            swap(array, largestItemIdx, lastUnsortedIdx);
+
+            --lastUnsortedIdx;
+        }
+
+        return array;
+    }
+
+    /**
      * Bubble sort sorts on array in asc order,
      *      by bubbling (moving) up larger values of the array to the end of the array.
      *
